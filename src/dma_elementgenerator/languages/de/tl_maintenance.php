f5d4a5560166805ea1e8c44ee30d20852b7b14ae
Michael Bischof <mb@byteworks.ch>
2017-01-16 21:21:15 +0100

    Added missing translation

--- src/dma_elementgenerator/languages/de/tl_maintenance.php
+++ src/dma_elementgenerator/languages/de/tl_maintenance.php
@@ -1,3 +1,4 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_maintenance_jobs']['dma_elementgenerator'][0] = 'Elementgenerator Cache';
+$GLOBALS['TL_LANG']['tl_maintenance_jobs']['dma_elementgenerator'][1] = 'Löschen des Elementgenerator Cache';
--- src/dma_elementgenerator/languages/en/tl_maintenance.php
+++ src/dma_elementgenerator/languages/en/tl_maintenance.php
@@ -1,3 +1,4 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_maintenance_jobs']['dma_elementgenerator'][0] = 'Element generator cache';
+$GLOBALS['TL_LANG']['tl_maintenance_jobs']['dma_elementgenerator'][1] = 'Clear the Element generator cache';
